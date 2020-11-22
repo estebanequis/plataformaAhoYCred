11d3546045f75edd2796f872c3cf9bfb79e11e0d
Esteban <estebanequis@gmail.com>
2020-11-21 22:10:20 -0300

    fin de primer dia de exitog

--- contracts/SavingAccount.sol
+++ contracts/SavingAccount.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.6.1;
 
 import './GeneralConfiguration.sol';
 
-pragma experimental ABIEncoderV2;
 contract SavingAccount {
     
     // el administrador no puede ser ni gestor ni auditor
@@ -86,27 +85,41 @@ contract SavingAccount {
         _;
     }
     
+    // constructor() public
+    // {   
+    //     administrador = msg.sender;
+    //     isActive = false;
+    // }
+
+    // function init(uint maxAhorristas, uint ahorroObj, string memory elObjetivo, uint minAporteDep, uint minAporteActivar, bool ahorristaVeAhorroActual, bool gestorVeAhorroActual) public
+    // {
+    //     require(maxAhorristas >= 6, "maxAhorristas must be greater than 5");
+    //     require(minAporteDep >= 0, "minAporteDep must be a positive value"); //ver si sirve
+    //     require(bytes(elObjetivo).length != 0, "elObjetivo must be specified");
+        
+    //     // administrador = msg.sender;
+    //     // isActive = false;
+    //     cantMaxAhorristas = maxAhorristas;
+    //     ahorroObjetivo = ahorroObj;
+    //     objetivo = elObjetivo;
+    //     minAporteDeposito = minAporteDep;
+    //     minAporteActivarCta = minAporteActivar;
+    //     ahorroActualVisiblePorAhorristas = ahorristaVeAhorroActual;
+    //     ahorroActualVisiblePorGestores = gestorVeAhorroActual;
+    // }
+    
+    //mock para pruebas
     constructor() public
     {   
         administrador = msg.sender;
-        isActive = false;
-    }
-
-    function init(uint maxAhorristas, uint ahorroObj, string memory elObjetivo, uint minAporteDep, uint minAporteActivar, bool ahorristaVeAhorroActual, bool gestorVeAhorroActual) public
-    {
-        require(maxAhorristas >= 6, "maxAhorristas must be greater than 5");
-        require(minAporteDep >= 0, "minAporteDep must be a positive value"); //ver si sirve
-        require(bytes(elObjetivo).length != 0, "elObjetivo must be specified");
-        
-        // administrador = msg.sender;
-        // isActive = false;
-        cantMaxAhorristas = maxAhorristas;
-        ahorroObjetivo = ahorroObj;
-        objetivo = elObjetivo;
-        minAporteDeposito = minAporteDep;
-        minAporteActivarCta = minAporteActivar;
-        ahorroActualVisiblePorAhorristas = ahorristaVeAhorroActual;
-        ahorroActualVisiblePorGestores = gestorVeAhorroActual;
+        isActive = true;
+        cantMaxAhorristas = 15;
+        ahorroObjetivo = 10000;
+        objetivo = "comprar una casa";
+        minAporteDeposito = 10;
+        minAporteActivarCta = 20;
+        ahorroActualVisiblePorAhorristas = true;
+        ahorroActualVisiblePorGestores = true;
     }
     
     function setConfigAddress( GeneralConfiguration _address ) public {
@@ -123,9 +136,42 @@ contract SavingAccount {
     
     //item 10 
     function sendDepositWithRegistration(string memory cedula, string memory fullName, address addressBeneficiario) public payable savingContractIsActive receiveMinDepositAmount {
+        uint elTime = block.timestamp;
+        uint monto = msg.value;
+        address addAux = msg.sender;
+        address addAux2 = addressBeneficiario;
+        
+        /*
+        string cedula;
+        string nombreCompleto;
+        uint fechaIngreso;
+        address cuentaEth;
+        address cuentaBeneficenciaEth;
+        uint montoAhorro;   //si isActive == false -> seria el dinero que no se suma al ahorro total del contrato
+        uint montoAdeudado;
+        bool isGestor;
+        bool isAuditor;
+        bool isActive;
+        bool isAproved;
+        */
+        
         if (!isInMapping(msg.sender))
         {
-            ahorristas[msg.sender] = Ahorrista(cedula, fullName, block.timestamp, msg.sender, addressBeneficiario, msg.value, 0, false, false, false, false);
+            //ahorristas[msg.sender] = Ahorrista(cedula, fullName, block.timestamp, msg.sender, addressBeneficiario, msg.value, 0, false, false, false, false);
+            //Ahorrista memory ahoAux = Ahorrista(cedula, fullName, elTime, addAux, addAux2, monto, 0, false, false, false, false);
+            ahorristas[addAux].cedula = cedula;
+            ahorristas[addAux].nombreCompleto = fullName;
+            ahorristas[addAux].fechaIngreso = elTime;
+            ahorristas[addAux].cuentaEth = addAux;
+            ahorristas[addAux].cuentaBeneficenciaEth = addAux2;
+            ahorristas[addAux].montoAhorro = monto;
+            ahorristas[addAux].montoAdeudado = 0;
+            ahorristas[addAux].isGestor = false;
+            ahorristas[addAux].isAuditor = false;
+            ahorristas[addAux].isActive = false;
+            ahorristas[addAux].isAproved = false;
+            
+            ahorristasIndex[cantAhorristas] = msg.sender;
             cantAhorristas++;
             if(msg.value >= minAporteActivarCta){
                 ahorristas[msg.sender].isActive = true;
@@ -152,7 +198,7 @@ contract SavingAccount {
         return ahorristas[unAddress].cuentaEth != address(0);
     }
     
-    function activateAhorrista(address unAddress) public onlyAuditor returns (bool) {
+    function aproveAhorrista(address unAddress) public onlyAuditor returns (bool) {
         bool retorno = false;
         
         if (isInMapping(unAddress) && ahorristas[unAddress].isActive == true) 
@@ -175,26 +221,8 @@ contract SavingAccount {
         return retorno;
     }
     
-    /*
-    function getMappingValue() public view returns (uint[] memory) {
-        uint[] memory memoryArray = new uint[](myVariable.sizeOfMapping);
-        for(uint i = 0; i < myVariable.sizeOfMapping; i++) {
-            memoryArray[i] = myVariable.myMappingInStruct[i];
-        }
-        return memoryArray;
-    }
-    
-    function name() public view returns(address[] memory) {
-        address[] memory playerList;
-        for(uint i=0; i<getPlayerCount(); i++) {
-            playerList[i] = players[i];
-        }
-        return playerList;
-    }
-    */
-    
     function getAhorristasToAprove() public onlyAuditor view returns(address[] memory) {
-        address[] memory losActivosSinAprobar;
+        address[] memory losActivosSinAprobar = new address[](cantAhorristasActivos - cantAhorristasAproved);
         uint j = 0;
         if (cantAhorristasActivos - cantAhorristasAproved > 0) 
         {
@@ -209,20 +237,35 @@ contract SavingAccount {
         return losActivosSinAprobar;
     }
     
-    function retornaBoolean(uint valor) public pure returns(bool) {
-        require(valor < 5, 'menor a 5 no funca');
-        if(valor<10){
-            return true;
-        }else{
-            return false;
-        }
-    } 
     
-    function probarRetorno(uint valor) public pure returns(bool){
-        bool retorno = retornaBoolean(valor);
-        
-        return retorno;
+    //////////////////////////////////////////
+    ////////// METODOS DE PRUEBA /////////////
+    //////////////////////////////////////////
+    
+    
+    function setRol(bool auditor, bool gestor) public {
+        ahorristas[msg.sender].isAuditor = auditor;
+        ahorristas[msg.sender].isGestor = gestor;
     }
+    
+    function getOneField() public view returns (address){
+        return ahorristas[msg.sender].cuentaEth;
+    }
+    
+    // function retornaBoolean(uint valor) public pure returns(bool) {
+    //     require(valor < 5, 'menor a 5 no funca');
+    //     if(valor<10){
+    //         return true;
+    //     }else{
+    //         return false;
+    //     }
+    // } 
+    
+    // function probarRetorno(uint valor) public pure returns(bool){
+    //     bool retorno = retornaBoolean(valor);
+        
+    //     return retorno;
+    // }
     
     // function getAhorristasToAprove() public onlyAuditor view returns (string[] memory){ 
     //     string[] memory losActivosSinAprobar = new string[](cantAhorristasActivos - cantAhorristasAproved);
@@ -241,5 +284,23 @@ contract SavingAccount {
         
     //     return losActivosSinAprobar;
     // }
+    
+     /*
+    function getMappingValue() public view returns (uint[] memory) {
+        uint[] memory memoryArray = new uint[](myVariable.sizeOfMapping);
+        for(uint i = 0; i < myVariable.sizeOfMapping; i++) {
+            memoryArray[i] = myVariable.myMappingInStruct[i];
+        }
+        return memoryArray;
+    }
+    
+    function name() public view returns(address[] memory) {
+        address[] memory playerList;
+        for(uint i=0; i<getPlayerCount(); i++) {
+            playerList[i] = players[i];
+        }
+        return playerList;
+    }
+    */
     
 }
